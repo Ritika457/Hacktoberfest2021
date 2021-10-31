0edc328da033e4de0ed86457be8b028d760f5027
JANAJITH D <56438939+Code-Theft@users.noreply.github.com>
2021-10-31 11:11:44 +0530

    added swap program

---
+++ swap.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main() {
+  double first, second, temp;
+  printf("Enter first number: ");
+  scanf("%lf", &first);
+  printf("Enter second number: ");
+  scanf("%lf", &second);
+
+  
+  temp = first;
+
+  
+  first = second;
+
+  second = temp;
+
+  // %.2lf displays number up to 2 decimal points
+  printf("\nAfter swapping, first number = %.2lf\n", first);
+  printf("After swapping, second number = %.2lf", second);
+  return 0;
+}
